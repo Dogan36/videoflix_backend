7af583a82d0bb2ec3cb64f201b78413773521b3a
Dogan Celik <dogancelik86@gmail.com>
2025-05-04 18:42:36 +0200

    Update STATIC_URL to use 'staticfiles/' for consistency with STATIC_ROOT

--- videoflix_backend/settings.py
+++ videoflix_backend/settings.py
@@ -200,7 +200,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'staticfiles/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 # Default primary key field type
